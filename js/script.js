e94283a71884eaa23bc0e7d40370002766bb8278
Patricia McPhee <hi@patriciamcphee.com>
2022-04-13 17:02:14 -0700

    add mini project

--- js/script.js
+++ js/script.js
@@ -1,41 +1,44 @@
 
 function newItem(){
 
-    //javascript
-    //1. Adding a new item to the list of items: 
-       let li = document.createElement("li");
-       let inputValue = document.getElementById("input").value;
-       let text = document.createTextNode(inputValue);
-       li.appendChild(text);
-    
-       if (inputValue === '') {
-         alert("You must write something!");
-       } else {
-         let list = document.querySelector('#list');
-         list.appendChild(li);
-       }
-    
-     //2. Crossing out an item from the list of items:
-       function crossOut() {
-             li.classList.toggle("strike");
-         }
-    
-         li.addEventListener("dblclick",crossOut);
-    
-     //3(i). Adding the delete button "X": 
-       let crossOutButton = document.createElement("crossOutButton");
-         crossOutButton.appendChild(document.createTextNode("X"));
-         li.appendChild(crossOutButton);
-    
-         crossOutButton.addEventListener("click", deleteListItem);
-     //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-       function deleteListItem(){
-             li.classList.add("delete")
-         }
-     // 4. Reordering the items: 
-       $('#list').sortable();
-    
-    }
+  // add a new item to list 
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+  
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+
+  // cross out item from list
+
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
+
+  // add delete button "x"
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", deleteListItem);
+
+  //add class delete display none to css
+  function deleteListItem(){
+    li.addClass("delete")
+  }
+
+  // add strikethrough on item in list
+  li.on("click", function() {
+    li.addClass("strike");
+  });
+
+  // reorder items in list 
+  $('#list').sortable();
+
+}
     
      
     
